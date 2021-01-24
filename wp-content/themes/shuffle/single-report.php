d64d784581a08283c3c7f1afa39c06ccaeb9c4ec
James Cliff <me@jamescliff.com>
2021-01-24 12:35:17 -0500

    :sparkles: updates

--- wp-content/themes/shuffle/single-report.php
+++ wp-content/themes/shuffle/single-report.php
@@ -14,80 +14,78 @@
       $obs = get_field('sr_obstacles');
       $post_date = get_the_date('F j, Y, g:i a');
     ?>
-    <div class="container">
-      <h1><?php the_title(); ?></h1>
-      
-      <?php if($postnum) : ?>
-        <h3>Post <?php echo $postnum; ?> Status Report</h3>
-      <p class="p-post-tags">
-      <?php endif; ?>
-        <?php $posttags = get_the_tags();
-        if ($posttags) {
-          foreach($posttags as $tag) {
-            echo $tag->name . ' '; 
-          }
+    <h1 class="p-main-content__heading"><?php the_title(); ?></h1>
+    
+    <?php if($postnum) : ?>
+      <h3>Post <?php echo $postnum; ?> Status Report</h3>
+    <p class="p-post-tags">
+    <?php endif; ?>
+      <?php $posttags = get_the_tags();
+      if ($posttags) {
+        foreach($posttags as $tag) {
+          echo $tag->name . ' '; 
         }
-        ?>
-      </p>
-      <pre class="p-post-date"><?php echo $post_date; ?></pre>
-        
-      <?php if($acc) : ?>
-        <h4>Accomplishments</h4>
-        <p><?php echo $acc; ?></p>
-      <?php endif; ?>
+      }
+      ?>
+    </p>
+    <pre class="p-post-date"><?php echo $post_date; ?></pre>
+      
+    <?php if($acc) : ?>
+      <h4>Accomplishments</h4>
+      <p><?php echo $acc; ?></p>
+    <?php endif; ?>
 
-      <h2 class="u-align-center">Total Hours: <?php echo $hrs; ?></h2>
+    <h2 class="p-main-content__hrs u-align-center">Total Hours: <?php echo $hrs; ?></h2>
 
-      <?php if($s_link) : ?>
-        <span>
-        <h5>Link to work:</h5>
-        <a href="<?php echo $s_link['url']; ?>" class="wp-block-button__link">
-          <?php echo $s_link['title']; ?>
+    <?php if($s_link) : ?>
+      <span>
+      <h5>Link to work:</h5>
+      <a href="<?php echo $s_link['url']; ?>" class="wp-block-button__link">
+        <?php echo $s_link['title']; ?>
+      </a>
+      </span>
+    <?php endif; ?>
+
+    <?php if($s_image) : ?>
+      <img src="<?php echo $s_image['url']; ?>" alt="<?php echo $s_image['alt']; ?>">
+      
+      <?php if($s_image['caption']) { ?>
+        <pre><?php echo $s_image['caption']; ?></pre>
+
+      <?php } elseif($s_image['description']) { ?>
+        <pre><?php echo $s_image['description']; ?></pre>
+
+      <?php } elseif($s_image['alt']) { ?>
+        <pre><?php echo $s_image['alt']; ?></pre>
+      <?php } ?>
+
+    <?php endif; ?>
+
+    <?php if($s_file) : ?>
+      <span>  
+        <h5>Uploaded file:</h5>
+        <a href="<?php echo $s_file['url']; ?>" class="wp-block-button__link">
+          <?php echo $s_file['title']; ?>
         </a>
-        </span>
-      <?php endif; ?>
+      </span>
+    <?php endif; ?>
 
-      <?php if($s_image) : ?>
-        <img src="<?php echo $s_image['url']; ?>" alt="<?php echo $s_image['alt']; ?>">
-        
-        <?php if($s_image['caption']) { ?>
-          <pre><?php echo $s_image['caption']; ?></pre>
+    <?php if($s_embed) : ?>
+      <?php echo $s_embed; ?>
+    <?php endif; ?>
 
-        <?php } elseif($s_image['description']) { ?>
-          <pre><?php echo $s_image['description']; ?></pre>
+    <?php if($plans) : ?>
+      <h4>Plan</h4>
+      <p><?php echo $plans; ?></p>
+    <?php endif; ?>
 
-        <?php } elseif($s_image['alt']) { ?>
-          <pre><?php echo $s_image['alt']; ?></pre>
-        <?php } ?>
+    <?php if($obs) : ?>
+      <h4>Obstacles</h4>
+      <p><?php echo $obs; ?></p>
+    <?php endif; ?>
 
-      <?php endif; ?>
+    <?php the_content(); ?>
 
-      <?php if($s_file) : ?>
-        <span>  
-          <h5>Uploaded file:</h5>
-          <a href="<?php echo $s_file['url']; ?>" class="wp-block-button__link">
-            <?php echo $s_file['title']; ?>
-          </a>
-        </span>
-      <?php endif; ?>
-
-      <?php if($s_embed) : ?>
-        <?php echo $s_embed; ?>
-      <?php endif; ?>
-
-      <?php if($plans) : ?>
-        <h4>Plan</h4>
-        <p><?php echo $plans; ?></p>
-      <?php endif; ?>
-
-      <?php if($obs) : ?>
-        <h4>Obstacles</h4>
-        <p><?php echo $obs; ?></p>
-      <?php endif; ?>
-
-      <?php the_content(); ?>
-
-    </div>
   <?php endwhile; ?>
 </main>
 <?php get_footer(); ?>
